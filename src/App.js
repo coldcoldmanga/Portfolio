791010c5b13e620cd52a80c4effdbad89768403a
Melvin <melvinkwanys@gmail.com>
2024-03-12 23:27:51 +0800

    changed some design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useState } from 'react';
 import { TypeAnimation }  from 'react-type-animation';
+import Profile from './profile.jsx';
 
 const App = () => {
 
-    const [textColor, setTextColor] = useState('red');
+    const [activePage, setPage] = useState(null);
+
+    const handlePage = (page) => {
+        setPage(page);
+    };
 
     return (
         <>
         <div className='container'>
+            <div className='screen'>
+                
+            </div>
             <div className='title'>
             <div><p className='intro'>// HI, I'M MELVIN KWAN, A...</p></div>
         
@@ -27,7 +35,7 @@ const App = () => {
                     wrapper="span"
                     cursor={true}
                     repeat={Infinity}
-                    style={{ fontSize: '4em', display: 'flex', justifyContent:'center', alignContent:'center', marginTop:'30px', color:'#00ADB5'}}
+                    style={{ fontSize: '4em', display: 'flex', justifyContent:'center', alignContent:'center', marginTop:'30px', color:'black'}}
                 />
                     <span className='fixed-curly'>{"  '}"}</span>
                 </h1>
@@ -35,27 +43,16 @@ const App = () => {
 
             </div>
 
-            <div className='content'>
-            <div className='card'>
-                <div className='card-content'>
-                    <nav></nav>
-                    <div></div>
-                    <footer></footer>
-                </div>
-            </div>
-
             <div className='button'>
-                    <div className='button-content'>
-                        <button>something</button>
-                    </div>
-            </div>
-
+                    <button className='profile-btn' onClick={() => handlePage('Profile')}>Profile</button>
+                    <button className='skillset-btn'onClick={() => handlePage('Skills')}>Skills</button>
+                    <button className='project-btn' onClick={() => handlePage('Project')}>Project</button>
             </div>
 
             
-        
-
         </div>
+
+        <footer className='footer'>©2024 - Melvin Kwan Yii Syn</footer>
             
         </>
 
---
+++ src/profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Content.css';
+
+const Profile = () => {
+
+    return (
+        <>
+        <h2 className='profile-header'>Introduction</h2>
+        <div className='profile-content'>
+        Greetings! I'm Melvin Kwan Yii Syn, a passionate individual hailing from the scenic landscapes of Sabah, Malaysia. Armed with a Bachelor's Degree in Security Technology from Multimedia University Malacca Campus, I embark on a journey fueled by innovation and dedication.
+
+My skill set encompasses a diverse range of languages and technologies, including HTML, CSS, JavaScript, PHP, MySQL, C++, Java, and Python. With a penchant for problem-solving and a commitment to excellence, I aim to leverage my expertise to create impactful solutions in the realm of technology.
+        </div>
+        </>
+    );
+}
+
+export default Profile;
