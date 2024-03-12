981b795f321fa07f0ab0e6b988eff608759a6f30
Melvin <melvinkwanys@gmail.com>
2024-03-12 18:00:00 +0800

    initial design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import { TypeAnimation }  from 'react-type-animation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [textColor, setTextColor] = useState('red');
+
+    return (
+        <>
+        <div className='container'>
+            <div className='title'>
+            <div><p className='intro'>// HI, I'M MELVIN KWAN, A...</p></div>
+        
+            <div>
+                <h1 className='role'>
+                    <span className='fixed-curly'>{"{'  "}</span>
+                    <TypeAnimation
+                    sequence={[
+                    '_Programmer', 
+                    2000, // Waits 1s
+                    '_Web Developer', 
+                    2000, // Waits 2s
+                    '_Sabahan', 
+                    2000,
+                    ]}
+                    wrapper="span"
+                    cursor={true}
+                    repeat={Infinity}
+                    style={{ fontSize: '4em', display: 'flex', justifyContent:'center', alignContent:'center', marginTop:'30px', color:'#00ADB5'}}
+                />
+                    <span className='fixed-curly'>{"  '}"}</span>
+                </h1>
+            </div>
+
+            </div>
+
+            <div className='content'>
+            <div className='card'>
+                <div className='card-content'>
+                    <nav></nav>
+                    <div></div>
+                    <footer></footer>
+                </div>
+            </div>
+
+            <div className='button'>
+                    <div className='button-content'>
+                        <button>something</button>
+                    </div>
+            </div>
+
+            </div>
+
+            
+        
+
+        </div>
+            
+        </>
+
+      
+    );
+  };
 
 export default App;
